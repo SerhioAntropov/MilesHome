ecd3c06a05918410cf2894b598261cb7fca09c52
Antropov Sergey <antropovsergey1991@gmail.com>
2023-04-01 11:49:41 +0300

    Initional commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        int ticket = 13776; //стоимость билета
+        int miles = 20; //количество рублей для одной бонусной мили
+
+        System.out.println("Вам начислено:");
+        System.out.println((ticket / miles) + " миль");
+    }
+}
